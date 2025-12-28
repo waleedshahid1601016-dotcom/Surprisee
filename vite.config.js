9ac209dfd45b31e90b414371b1717dd4fcb4403c
waleedshahid1601016-dotcom <waleedshahid1601016@gmail.com>
2025-12-28 23:36:29 +0500

    fixed

--- vite.config.js
+++ vite.config.js
@@ -1,3 +1,4 @@
+/* eslint-disable no-undef */
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
@@ -5,4 +6,5 @@ import tailwindcss from '@tailwindcss/vite'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
+  base: process.env.VITE_BASE_PATH || "/Surprisee",
 })
